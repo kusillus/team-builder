e0d31b6012d68ac11725022b8670236cf58a9bc0
Wilder Alexander Guevara Benites <wguevarab7@gmail.com>
2019-10-09 18:34:47 -0500

    first commit

---
+++ plugins/firebase.js
@@ -0,0 +1,30 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+
+
+// import firebase from 'firebase/app'
+// import 'firebase/firestore'
+
+if (!firebase.apps.length) {
+    const firebaseConfig = {
+        apiKey: "AIzaSyDhcecvsr-rQBQU7AMaEri-Tdj8o6303d0",
+        authDomain: "ks-team-builder.firebaseapp.com",
+        databaseURL: "https://ks-team-builder.firebaseio.com",
+        projectId: "ks-team-builder",
+        storageBucket: "ks-team-builder.appspot.com",
+        messagingSenderId: "218291553561",
+        appId: "1:218291553561:web:7ca63e0ba2df6c3a577188",
+        measurementId: "G-XK1L15JWHD"
+      };
+
+    firebase.initializeApp(firebaseConfig)
+}
+
+const fireDb = firebase.firestore()
+
+export { fireDb }
